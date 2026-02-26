fa97a99fd2a577d9e95332100f76b2ab562b894c
Kaspar Birk <kaspar.birk@gmail.com>
2026-02-26 15:49:21 +0100

    fix redirect loop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
 	import { onMount } from 'svelte';
 
-	onMount(async () => {
-		goto(`${base}/users.html`);
+	onMount(() => {
+		window.location.replace(`${base}/users.html`);
 	});
 </script>
